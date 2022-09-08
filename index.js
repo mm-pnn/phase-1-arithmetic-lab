511b961d66bcac88655b244f2d73d2fb04d8448c
Melanie Penn <melanie.penn17@gmail.com>
2022-09-08 06:33:09 -0500

    commit tree 675bea156a72735531701ebf3d0e094f76e6c7cb

--- index.js
+++ index.js
@@ -1,2 +1,13 @@
 
-// Write your code here
+
+let num1 = 2
+let num2 = 31
+let multiply = num1 * num2;
+
+let random = Math.ceil(Math.random());
+
+const num3 = 16;
+const num4 = 6
+let mod = num3 % num4;
+
+const max = Math.max( 5, 10, 15, 20 );
